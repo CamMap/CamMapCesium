cb0a3da3b9b976a200ea5d20ab6542ee5dade048
Richard Menzies <2383425m@student.gla.ac.uk>
2020-11-07 21:51:58 +0000

    Add new eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,8 +9,98 @@ module.exports = {
         'plugin:@typescript-eslint/recommended',
     ],
     "rules": {
-        "semi": "error",
-        "indent": ["error", 4],
+        "semi": "off",
+        "@typescript-eslint/semi": ["error"],
+
+        "indent": "off",
+        "@typescript-eslint/indent": ["error", 4],
+
+        "no-loop-func": "off",
+        "@typescript-eslint/no-loop-func": ["warn"],
+
+        "func-call-spacing": "off",
+        "@typescript-eslint/func-call-spacing": ["error", "never"],
+
+        "keyword-spacing": "off",
+        "@typescript-eslint/keyword-spacing": ["error", {
+            "before": true,
+            "after": true,
+            "overrides":{
+                "if":{
+                    "after": false
+                },
+                "for":{
+                    "after": false
+                },
+                "while":{
+                    "after": false
+                }
+            }
+        }],
+
+        "function-paren-newline": ["error", { "minItems": 4 }],
+        "eol-last": ["error", "always"],
+        "no-trailing-spaces": ["error"],
+        "capitalized-comments": ["error"],
+        "no-lonely-if": ["error"],
+        "linebreak-style": ["error", "unix"],
+        "func-style": ["error", "declaration"],
+        "no-unneeded-ternary": ["error"],
+        "one-var": ["error", "never"],
+        "prefer-object-spread": ["error"],
+        "sort-imports": ["error"],
+        "space-infix-ops": ["error"],
+        "space-unary-ops": ["error", {"words": true, "nonwords": false}],
+        "switch-colon-spacing": ["error", {"after": true, "before": false}],
+        "space-in-parens": ["error", "never"],
+        "semi-style": ["error", "last"],
+        "padded-blocks": ["error", "never"],
+        "operator-assignment": ["error", "always"],
+        "no-unreachable": "error",
+        "no-unreachable-loop": "error",
+        "getter-return": ["error", { "allowImplicit": false }],
+        "comma-style": ["error", "last"],
+        "comma-spacing": ["error", { "before": false, "after": true }],
+
+        "no-redeclare": "off",
+        "@typescript-eslint/no-redeclare": ["error"],
+
+        "no-dupe-class-members": "off",
+        "@typescript-eslint/no-dupe-class-members": ["error"],
+
+        "no-duplicate-imports": "off",
+        "@typescript-eslint/no-duplicate-imports": ["error"],
+
+        "no-empty-function": "off",
+        "@typescript-eslint/no-empty-function": ["error"],
+
+        "no-unused-vars": "off",
+        "@typescript-eslint/no-unused-vars": ["error"],
+
+        "quotes": "off",
+        "@typescript-eslint/quotes": ["error", "double"],
+
+        "space-before-function-paren": "off",
+        "@typescript-eslint/space-before-function-paren": ["error", "never"],
+
         "no-multiple-empty-lines": ["error", { "max": 2, "maxEOF": 1, "maxBOF": 0 }],
+
+        "no-extra-parens": "off",
+        "@typescript-eslint/no-extra-parens": ["error"],
+
+        "no-extra-semi": "off",
+        "@typescript-eslint/no-extra-semi": ["error"],
+
+        "no-magic-numbers": "off",
+        "@typescript-eslint/no-magic-numbers": ["error", { "ignoreEnums": true }],
+
+        "lines-between-class-members": "off",
+        "@typescript-eslint/lines-between-class-members": ["error", "always", { "exceptAfterSingleLine": true }],
+
+        "comma-dangle": "off",
+        "@typescript-eslint/comma-dangle": ["error", "always-multiline"],
+
+        "brace-style": "off",
+        "@typescript-eslint/brace-style": ["error", "1tbs"],
     }
 };
