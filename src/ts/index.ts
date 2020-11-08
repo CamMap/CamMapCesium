2d0aa41e71903f0c2e5f93fdb4de0ac5bda73887
Alex Tsernoh <foralwork@gmail.com>
2020-11-08 18:42:14 +0200

    implement viewmodel structure and placeholders

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,6 +1,29 @@
+import * as Cesium from "cesium_source/Cesium";
+import CameraViewModel from './cameraViewModel';
+
+//TODO: someone migrate this to ES6 later on
 require('../css/main.css');
 require('cesium_source/Widgets/widgets.css');
 require('./image');
 
-// Run example 2
-require('./examples/ex2');
+// setup cesium root panel reference;
+const cesiumRoot = new Cesium.Viewer('cesiumContainer', {
+    animation: false,
+    timeline: false,
+    geocoder: false,
+    selectionIndicator: false,
+    infoBox: false,
+    vrButton: false,
+    fullscreenButton: false
+});
+
+// set default view
+cesiumRoot.camera.setView({
+    destination: Cesium.Cartesian3.fromDegrees(-4.291606, 55.873704, 30.0),
+    orientation: {
+        heading: Cesium.Math.toRadians(0.0),
+        pitch: Cesium.Math.toRadians(-45.0),
+    }
+});
+
+const viewModel = new CameraViewModel(cesiumRoot);
